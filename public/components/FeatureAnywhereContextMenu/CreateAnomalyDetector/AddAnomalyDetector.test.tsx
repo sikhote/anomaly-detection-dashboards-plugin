257052c69199d3ec9376574cffd945282aa54103
David Sinclair <dsincla@rei.com>
2023-05-26 14:22:32 -0700

    start on unit test fix
    Signed-off-by: David Sinclair <dsincla@rei.com>

--- public/components/FeatureAnywhereContextMenu/CreateAnomalyDetector/AddAnomalyDetector.test.tsx
+++ public/components/FeatureAnywhereContextMenu/CreateAnomalyDetector/AddAnomalyDetector.test.tsx
@@ -9,32 +9,45 @@ import AddAnomalyDetector from '../CreateAnomalyDetector';
 import { Formik } from 'formik';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import configureStore from '../../../redux/configureStore'
+
+const store = configureStore();
+
+const ReduxProvider = ({ children }) => (
+  <Provider store={store}>{children}</Provider>
+);
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('AddAnomalyDetector', () => {
   test('renders', () => {
-    const wrapper = shallow(<AddAnomalyDetector />);
+    const wrapper = shallow(
+      <ReduxProvider>
+        <AddAnomalyDetector />
+      </ReduxProvider>
+    );
     expect(wrapper).toMatchSnapshot();
   });
   test('show errors for empty detector name field', async () => {
     const { findByText } = render(
-      <Formik initialValues={{ name: '' }} onSubmit={jest.fn()}>
-        {() => (
-          <div>
-            <AddAnomalyDetector 
-              embeddable={jest.fn()} 
-              closeFlyout={jest.fn()} 
-              mode={jest.fn()} 
-              setMode={jest.fn()} 
-              selectedDetector={jest.fn()}
-              setSelectedDetector={jest.fn()}/>
-          </div>
-        )}
-      </Formik>
+      <ReduxProvider>
+        <Formik initialValues={{ name: '' }} onSubmit={jest.fn()}>
+          {() => (
+            <div>
+              <AddAnomalyDetector
+                embeddable={jest.fn()}
+                closeFlyout={jest.fn()}
+                mode={jest.fn()}
+                setMode={jest.fn()}
+                selectedDetector={jest.fn()}
+                setSelectedDetector={jest.fn()}
+              />
+            </div>
+          )}
+        </Formik>
+      </ReduxProvider>
     );
     expect(findByText('Required')).not.toBeNull();
   });
 });
-
-
